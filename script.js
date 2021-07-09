e53dc882016432452cb7ea803873ccb970b8570f
Harborth, Allen <allenharborth9835@gmail.com>
2021-07-08 21:13:21 -0500

    add basic processes and functions to make password, code works as needed

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 var generateBtn = document.querySelector("#generate");
 
 //create array with different options
-var numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var specialChar = [ " ", "!", "#", "$", "%", "&", "’", "(", ")", "+", ",", "-", ".", "/", ":" , ";", "<", "=", ">", "?", "@","[","\\",	"]",	"^", "_",	"`",	"{",	"|",	"}",	"~", "\""]
+var numbersArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = [ " ", "!", "#", "$", "%", "&", "’", "(", ")", "+", ",", "-", ".", "/", ":" , ";", "<", "=", ">", "?", "@","[","\\",	"]",	"^", "_",	"`",	"{",	"|",	"}",	"~", "\""];
 //create a function to pick a random array element
 var pickRandom = function(array){
   var randomPick = Math.floor(Math.random() * array.length);
-  return randomPick;
+  return randomPick
 }
 
 //create a prompt to ask for length of password
@@ -24,77 +24,101 @@ var getLength = function(){
     window.alert("length to long please try again!");
     getLength();
   }else{
-    console.log(length);
-    return length;
+    return length
   }
 }
 var getOptions = function(){
-  let bulkArray = [];
-  let essentailArray = [];
-  let numOfArrays = 0;
-  //promp to ask if it should contain special characters
-  var specialCase = window.confirm("do you want special characters?");
-  if(specialCase){
-    numOfArrays ++;
-    bulkArray=bulkArray.concat(specialChar);
-    essentailArray=essentailArray.concat(pickRandom(specialChar));
-  
-  }
-  //prompt to ask if it should contain uppercase letters
-  var upperCase = window.confirm("do you want uppercase letters?");
-  if(upperCase){
-    numOfArrays ++;
-    bulkArray=bulkArray.concat(upperCaseArr);
-    essentailArray=essentailArray.concat(pickRandom(upperCaseArr));
-  }
-  //prompt to ask if it should contain lowercase letters
-  var lowerCase = window.confirm("do you want lowercase letters?");
-  if(lowerCase){
-    numOfArrays ++;
-    bulkArray=bulkArray.concat(lowerCaseArr);
-    essentailArray=essentailArray.concat(pickRandom(lowerCaseArr));
+  var bulkArray = []
+  var essentailArray = []
+  var numOfArrays = 0
+    //promp to ask if it should contain special characters
+    var specialCase = window.confirm("do you want special characters?")
+    if(specialCase){
+      bulkArray=bulkArray.concat(specialChar)
+      essentailArray=essentailArray.concat(
+        specialChar[pickRandom(specialChar)]
+      )
+      numOfArrays ++
+    }
+    //prompt to ask if it should contain uppercase letters
+    var upperCase = window.confirm("do you want uppercase letters?")
+    if(upperCase){
+      bulkArray=bulkArray.concat(upperCaseArr)
+      essentailArray=essentailArray.concat(
+        upperCaseArr[pickRandom(upperCaseArr)]
+      )
+      numOfArrays ++
+    }
+    //prompt to ask if it should contain lowercase letters
+    var lowerCase = window.confirm("do you want lowercase letters?")
+    if(lowerCase){
+      bulkArray=bulkArray.concat(lowerCaseArr)
+      essentailArray=essentailArray.concat(
+        lowerCaseArr[pickRandom(lowerCaseArr)]
 
-  }
-  //prompt to ask if it should contain numbers
-  var numbers = window.confirm("do you want numbers?");
-  if(numbers){
-    numOfArrays ++;
-    bulkArray=bulkArray.concat(numbersArr);
-    essentailArray=essentailArray.concat(pickRandom(numbersArr));
-
-  }
-  //if no category is chosen it prompts to chose at least one option
-  if(numOfArrays === 0){
-    window.alert("you must pick at least one category!!!")
-    getOptions();
-  }
-  return [bulkArray, essentailArray];
+      )
+      numOfArrays ++
+    }
+    //prompt to ask if it should contain numbers
+    var numbers = window.confirm("do you want numbers?")
+    if(numbers){
+      bulkArray=bulkArray.concat(numbersArr)
+      essentailArray=essentailArray.concat(
+        numbersArr[pickRandom(numbersArr)]
+      )
+      numOfArrays ++
+    }
+    //if no category is chosen it prompts to chose at least one option
+    if(numOfArrays === 0){
+      window.alert("you must pick at least one category!!!")
+      return getOptions()
+    }else{
+      return [bulkArray, essentailArray];
+    }
 }
 var generatePassword = function(essentailArr, bulkArr, passwordLength){
-  let password = [];
-  var charNum = 0;
-  while(charNum != passwordLength){
-    if(essentailArr.length>0){
-
+  let password = "";
+  var pass = 0;
+  console.log(essentailArr)
+  for(var i = 0; i!=passwordLength; i++){
+    if(essentailArr.length + i == passwordLength){
+      var arrayNum = pickRandom(essentailArr)
+      var pick = essentailArr[arrayNum]
+      password = password + pick
+      essentailArr.splice(arrayNum, 1)
+      //console.log(essentailArr.length)
+      pass++
+    }else if(essentailArr.length > 0){
+      if(Math.random()>= 0.5){
+        var arrayNum = pickRandom(essentailArr)
+        var pick = essentailArr[arrayNum]
+        password = password + pick
+        essentailArr.splice(arrayNum, 1)
+        //console.log(essentailArr.length)
+        pass++
+      }else{
+        var pick = bulkArr[pickRandom(bulkArr)]
+        password = password + pick
+      }
     }else{
-      var pick = pickRandom(bulkArr);
-      password = password.concat(pick);
+      var pick = bulkArr[pickRandom(bulkArr)]
+      password = password + pick
     }
   }
+  console.log(pass)
+  return password
 }
 function writePassword() {
-  var codeLength = getLength();
-  var codeArrays = getOptions();
-  var bulk = codeArrays[0];
-  var essent = codeArrays[1];
-  window.alert(bulk.length)
-  window.alert(essent.length)
+  var codeLength = getLength()
+  var codeArrays = getOptions()
+  var bulk = codeArrays[0]
+  var essent = codeArrays[1]
  
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-  var password = generatePassword(essent, bulk, codeLength);
+  var passwordText = document.querySelector("#password")
+  var password = generatePassword(essent, bulk, codeLength)
+  passwordText.value = password
 }
   // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+  generateBtn.addEventListener("click", writePassword)
 
 
